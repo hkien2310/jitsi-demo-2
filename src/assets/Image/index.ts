d1fdafd220bdd8c34e41f14304ad2c2360571866
yenlinh9700@gmail.com <yenlinh9700@gmail.com>
2024-04-01 11:32:23 +0700

    add icon

--- src/assets/Image/index.ts
+++ src/assets/Image/index.ts
@@ -3,6 +3,18 @@ import PDFLogo from './source/pdf.png'
 import OtherLogo from './source/other.png'
 import ImageLogo from './source/img.png'
 import excelLogo from './source/xls.png'
+import searchIcon from './source/search_normal.svg'
+import arrowDown from './source/arrow_down.svg'
+import arrowLeft from './source/arrow_left.svg'
+import arrowRight from './source/arrow_right.svg'
+import arrowUp from './source/arrow_up.svg'
+import bag from './source/bag.svg'
+import close from './source/close.svg'
+import documentUpload from './source/document_upload.svg'
+import login from './source/login.svg'
+import notification from './source/notification.svg'
+import searchNormal from './source/search_normal.svg'
+import warning from './source/warning.svg'
 
 export const ImageSource = {
     loginAvatar: 'https://sentrangusa.com/wp-content/uploads/2023/01/62196607bf1b46ebfb301f19_5f5bc69416e7fb495275b378_how20run20tips20top20execs.jpeg',
@@ -13,4 +25,16 @@ export const ImageSource = {
     xslLogo: excelLogo,
     otherLogo: OtherLogo,
     imgLogo: ImageLogo,
+    searchIcon,
+    arrowDown,
+    arrowLeft,
+    arrowRight,
+    arrowUp,
+    bag,
+    close,
+    documentUpload,
+    login,
+    notification,
+    searchNormal,
+    warning
 }
